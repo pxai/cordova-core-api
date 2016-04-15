2c28c2a5e44964e2f1ee78409177012c246b5a0c
pxai <pello_altadill@cuatrovientos.org>
2016-04-15 17:11:12 +0200

    First commit with all plugins0

---
+++ installall.sh
@@ -0,0 +1,18 @@
+cordova plugin add cordova-plugin-battery-status
+cordova plugin add cordova-plugin-camera
+cordova plugin add cordova-plugin-console
+cordova plugin add cordova-plugin-contacts
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-device-motion and
+cordova plugin add cordova-plugin-device-orientation
+cordova plugin add cordova-plugin-dialogs
+cordova plugin add cordova-plugin-file and cordova-pluginfile-transfer
+cordova plugin add cordova-plugin-geolocation
+cordova plugin add cordova-plugin-globalization
+cordova plugin add cordova-plugin-inappbrowser
+cordova plugin add cordova-plugin-media and cordova-pluginmedia-capture
+cordova plugin add cordova-plugin-network-information
+cordova plugin add cordova-plugin-splashscreen
+cordova plugin add cordova-plugin-vibration
+cordova plugin add cordova-plugin-statusbar
+cordova plugin add cordova-plugin-whitelist
