4b7bb7a36fa7f7e7a72948bd9545ef5ad9198b34
pello_altadill <pello_altadill@cuatrovientos.org>
2016-05-03 08:52:03 +0200

    ok, added bootstrap and battery sample

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cordova-core-api
 Samples using cordova core api
+# Platforms
+# Build and run cycle
+Add to platform
+
+Build the app
+
+```
+cordova build browser
+```
+
+# Run the app
+```
+cordova run browser
+```
--- www/js/batteries.js
+++ www/js/batteries.js
@@ -4,14 +4,14 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * 'License'); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
@@ -34,6 +34,33 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        // Shows battery status:
+        window.addEventListener('batterystatus', onBatteryStatus, false);
+
+        function onBatteryStatus(status) {
+            var msg = 'Level: ' + status.level + ' isPlugged: ' + status.isPlugged;
+            $('#status').html(msg);
+            console.log(msg);
+        }
+
+        // Battery low event
+        window.addEventListener('batterylow', onBatteryLow, false);
+
+        function onBatteryLow(status) {
+            var msg = 'Battery Level Low ' + status.level + '%';
+            $('#status').html(msg);
+            console.log(msg);
+        }
+
+        // Battery critical
+        window.addEventListener('batterycritical', onBatteryCritical, false);
+
+        function onBatteryCritical(status) {
+            var msg = 'Battery Level Critical ' + status.level + '%\nRecharge Soon!';
+            $('#status').html(msg);
+            console.log(msg);
+        }
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
