b5d7325f496724a5c072c7406a30c3d6637a40fb
pxai <pello_altadill@cuatrovientos.org>
2016-05-10 00:27:38 +0200

    Ok, added skels

--- www/js/fileSystem.js
+++ www/js/fileSystem.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        console.log('Trying file system');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
